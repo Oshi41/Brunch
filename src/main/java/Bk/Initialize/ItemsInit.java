36c6d7222a4d0429b395c1421e909f9bbf759fd2
Oshi41 <sheglovaoe@gmail.com>
2017-06-24 12:03:55 +0300

    Commit

--- src/main/java/Bk/Initialize/ItemsInit.java
+++ src/main/java/Bk/Initialize/ItemsInit.java
@@ -1,19 +1,30 @@
 package Bk.Initialize;
 
 import Bk.Base.BaseVanilla.BkItem;
+import Bk.Base.Tools.Hammer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by User on 22.06.2017.
  */
 public class ItemsInit  {
 
+    ArrayList<Class> classes = new ArrayList<Class>(Arrays.asList(
+            BkItem.class
+    ));
+
     //region Fields
+    public static Hammer hammes;
     //endregion
 
     public static void init(){
-
+        hammes = register(new Hammer(Hammer.Hammers.BUILDER_HAMMER));
     }
 
     //region Helping Method
@@ -25,6 +36,16 @@ public class ItemsInit  {
         }
 
         return item;
+    }
+
+    private static <T extends Item> T registerWithSubtypes(T item){
+        NonNullList<ItemStack> items = NonNullList.create();
+
+        item.getSubItems(item, item.getCreativeTab(), items);
+        for(ItemStack stack : items){
+
+        }
+        return register(item);
     }
     //endregiuon
 }
