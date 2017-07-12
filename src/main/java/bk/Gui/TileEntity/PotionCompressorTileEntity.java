cd379cacccc3c9e5c10934cb8dac4e6c93dd8955
Oshi41 <sheglovaoe@gmail.com>
2017-07-13 00:14:02 +0300

    Potion Compressor

--- src/main/java/bk/Gui/TileEntity/PotionCompressorTileEntity.java
+++ src/main/java/bk/Gui/TileEntity/PotionCompressorTileEntity.java
@@ -75,7 +75,7 @@ public class PotionCompressorTileEntity extends BaseMachineUpgradeTileEntity {
     
     @Override
     public String getName() {
-        return null;
+        return "bk:potioncopressor";
     }
     
     @Override
